ab01e4868a0a81d6ddc904f9747b7db2a1592780
Hyaku <hyakusbubble@gmail.com>
2019-02-22 13:17:38 +0100

    Setting up waypoint.cs
    does not work (V1)

--- Soundscape Immersif/Assets/Scripts/waypoints.cs
+++ Soundscape Immersif/Assets/Scripts/waypoints.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class waypoints : MonoBehaviour {
+    public GameObject viewfinder;
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    private Vector2 viewfinder_pos;
+    private Vector2 waypoint1_pos;
+    private Vector2 waypoint2_pos;
+    public float speed = 0.3f;
+    int seconds;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    IEnumerator WaitTime(int seconds)
+    {   
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Enter Wait Time");
+    }
+
+    // Use this for initialization
+    void Start () {
+        waypoint1_pos = new Vector2(waypoint1.x, waypoint1.y);
+        waypoint2_pos = new Vector2(waypoint2.x, waypoint2.y);
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        viewfinder_pos = viewfinder.transform.position;
+        float step = speed * Time.deltaTime;
+
+        if (viewfinder_pos != waypoint1_pos && viewfinder_pos != waypoint1_pos)
+            {
+            viewfinder_pos = Vector2.MoveTowards(viewfinder_pos, waypoint1_pos, step);
+            Debug.Log("Premier if");
+            }
+
+        else if (viewfinder_pos == waypoint1_pos && viewfinder_pos == waypoint1_pos)
+        {
+            WaitTime(3);
+            viewfinder_pos = Vector2.MoveTowards(viewfinder_pos, waypoint2_pos, step);
+            Debug.Log("Success ?");
+        }
+    }
 }
