08b61ef7611033cc848ecd3c0dac8782e1c8db5b
Hyaku <hyakusbubble@gmail.com>
2019-02-22 16:31:18 +0100

    Play a sound when entering waypoint collider - working
    Put the script on the waypoint + a sphere collider & audiosource :
    - no play on awake
    - no loop
    - no spacialization

---
+++ Soundscape Immersif/Assets/Scripts/trigger_oneshot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger_oneshot : MonoBehaviour {
+
+    AudioSource m_MyAudioSource;
+
+    // Use this for initialization
+    void Start () {
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       m_MyAudioSource.Play();
+        Debug.Log("Played");
+    }
+}
